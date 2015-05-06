90c518759278f27df074c0cb50612773683ab0d1
Hilde Vestol <hilde@rubynor.com>
2015-05-06 12:02:27 +0200

    Update summary

--- lib/macbot/metadata.rb
+++ lib/macbot/metadata.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module Macbot
   VERSION = '0.1.4'
   NAME = 'Macbot'
-  SUMMARY = 'Set it up to disable/enable accounts'
+  SUMMARY = 'Set it up to disable/enable email accounts in Mail for OSX'
   YAML_PATH = 'email.yml'
   EMAIL_GROUPS = if File.exists? Macbot::YAML_PATH
                    YAML.load_file(Macbot::YAML_PATH).keys
